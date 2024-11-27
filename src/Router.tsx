12cf8e3884a965726147c285129df24c1df7277f
Balconywood <davidcannon789@gmail.com>
2024-11-27 04:19:09 +0000

    feat: Add PageLayout component to display Header at top of every page

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,16 +4,21 @@ import HomePage from './pages/HomePage';
 import ProjectPage from './pages/ProjectPage';
 import CollaborativeDataLabPage from './pages/CollaborativeDataLabPage';
 import DataExplorationPage from './pages/DataExplorationPage';
+import PageLayout from './components/PageLayout';
 
 export default function Router() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route index element={<HomePage />} />
-                <Route path="projects/:projectId">
-                    <Route index element={<ProjectPage />} />
-                    <Route path="collaborative" element={<CollaborativeDataLabPage />} />
-                    <Route path="exploration" element={<DataExplorationPage />} />
+                {/* Wrap all site routes with our PageLayout component, adding
+                    the header on each page */}
+                <Route element={<PageLayout />}>
+                    <Route index element={<HomePage />} />
+                    <Route path="projects/:projectId">
+                        <Route index element={<ProjectPage />} />
+                        <Route path="collaborative" element={<CollaborativeDataLabPage />} />
+                        <Route path="exploration" element={<DataExplorationPage />} />
+                    </Route>
                 </Route>
             </Routes>
         </BrowserRouter>
