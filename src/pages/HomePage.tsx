143c5bab617c1fc3e215daf01354d4cf70f270fd
2656327s <2656327s@student.gla.ac.uk>
2024-11-28 18:45:44 +0000

    homepage setup

--- src/pages/HomePage.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default function HomePage() {
-    return (
-        <p>This is our home page!</p>
-    );
-}
--- src/pages/ProjectPage.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default function ProjectPage() {
-    return (
-        <p>This is our project page!</p>
-    );
-}
